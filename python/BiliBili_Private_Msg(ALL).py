4d16898acf56ea374b2a33321a8e9f145304e596
hd80606b <42578272+hd80606b@users.noreply.github.com>
2023-08-19 01:16:38 +0300

    Add BiliBili_Private_Msg(ALL).py
    It is used to copy all the Private_Msg to a local folder

---
+++ python/BiliBili_Private_Msg(ALL).py
@@ -0,0 +1,87 @@
+import requests
+import json
+import datetime
+import time
+import os
+
+def timestamp_to_datetime(unix_timestamp):
+    start_time = datetime.datetime(1970, 1, 1).replace(tzinfo=datetime.timezone.utc)
+    dt = start_time + datetime.timedelta(seconds=unix_timestamp)
+    local_dt = dt.astimezone(datetime.timezone(datetime.timedelta(hours=8))) # 当地时区
+    local_dt_str = local_dt.strftime('%Y/%m/%d %H:%M:%S:%f')
+    local_dt_str = local_dt_str[:-7]
+    return  local_dt_str
+    
+def extract_content(data):
+    try:
+        content_data = json.loads(data)
+        if isinstance(content_data, dict) and "content" in content_data:
+            return content_data["content"]
+        else:
+            return data
+    except json.JSONDecodeError:
+        return data
+       
+param1 = input("请输入Cookie: ")
+end_list = 0
+end = 0
+# 构造Cookie请求头
+cookies = {
+    "param1": param1
+}
+
+messages = []  # 保存所有的消息
+talker_ids = []  # 用于存储提取的talker_id
+current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # 获取当前时间作为文件夹名
+folder_path = os.path.join(".", current_time)  # 当前目录
+
+# 拿列表
+while(1):
+    url = "https://api.vc.bilibili.com/session_svr/v1/session_svr/get_sessions?session_type=1&group_fold=1&unfollow_fold=0&sort_rule=2&end_ts="+str(end_list)+"&build=0&mobi_app=web&size=200"
+    response = requests.get(url, cookies=cookies)
+    # 解析JSON数据
+    parsed_data = json.loads(response.text)
+    List_messages = parsed_data["data"]["session_list"]
+    if not List_messages:
+        break
+    for session in List_messages:
+        talker_id = session["talker_id"]
+        talker_ids.append(talker_id)
+    last_session = List_messages[-1]
+    end_list = last_session["last_msg"]["timestamp"]
+
+# 创建文件夹
+os.makedirs(folder_path, exist_ok=True)
+
+# 使用保存的talker_ids进行GET请求
+for talker_id in talker_ids: 
+    file_name = str(talker_id) +".txt"
+    file_path = os.path.join(folder_path, file_name)
+    while(1):
+        # 发送GET请求
+        url = "https://api.vc.bilibili.com/svr_sync/v1/svr_sync/fetch_session_msgs?size=200&build=0&mobi_app=web&begin_seqno=0&end_seqno="+str(end)+"&sender_device_id=1&talker_id="+ str(talker_id) + "&session_type=1"
+        response = requests.get(url, cookies=cookies)
+        # 解析JSON数据
+        parsed_data = json.loads(response.text)
+        new_messages = parsed_data["data"]["messages"]
+        if not new_messages:
+            break
+        end = parsed_data["data"]["min_seqno"]
+        messages.extend(new_messages)  # 将新消息添加到消息列表中
+        with open(file_path, 'w', encoding='utf-8') as f:
+            for message in reversed(messages):
+                content = message["content"]
+                if isinstance(content, int):
+                    content = str(content)
+                content = extract_content(content)
+                content = content.replace('\n', ' ')
+                UID = json.loads(json.dumps(message["sender_uid"]))
+                Timestamp = json.loads(json.dumps(message["timestamp"]))
+                f.write(str(timestamp_to_datetime(Timestamp))+'\x20'+str(UID) + '说：\x20' + content + '\n')
+    print("数据已保存到"+str(talker_id)+'.txt中')
+    messages.clear()
+    end = 0
+    #time.sleep(2)  # 这里延时 2 秒
+print("数据已全部保存完毕")
+
+
